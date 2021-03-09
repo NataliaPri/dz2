a8b26bda2be8bfc4ebdc82e44ea30cc4584c1fac
NataliaPri <77389496+NataliaPri@users.noreply.github.com>
2021-03-09 19:54:38 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+
+public class User
+{
+
+
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        var user = obj as User;
+
+        if (user == null)
+            return false;
+
+        return FirstName == user.FirstName && SecondName == user.SecondName;
+    }
+
+    public override int GetHashCode()
+    {
+        int firtsNameHashCode = FirstName?.GetHashCode() ?? 0;
+        int secondNameHashCode = SecondName?.GetHashCode() ?? 0;
+        return firtsNameHashCode ^ secondNameHashCode;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var hashSet = new HashSet<User>();
+
+        var user = new User() { FirstName = "Barbara", SecondName = "Santa" };
+
+        hashSet.Add(user);
+
+        var searchUsser = new User() { FirstName = "Barbara", SecondName = "Santa" };
+
+        Console.WriteLine($"contains user {hashSet.Contains(user)}, contains searchUsser {hashSet.Contains(searchUsser)}");
+
+
+
+    }
+}
